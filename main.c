7d3c6f00152f4e5fe5f277b538db6c965a30cfd3
bdnugget <bdbosman@gmail.com>
2024-05-21 00:04:17 +0200

    clang-format

--- main.c
+++ main.c
@@ -10,159 +10,151 @@ void drawAtom(Atom atom);
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 1200;
-    const int screenHeight = 800;
+int main(void) {
+  // Initialization
+  //--------------------------------------------------------------------------------------
+  const int screenWidth = 1200;
+  const int screenHeight = 800;
 
-    // Molecule mol = parseSDF("./resources/mol.sdf");
-    Molecule mol = parseSDF("./resources/methyl-vinyl-ketone.sdf");
-    // normalizeCoordinates(&mol, screenWidth, screenHeight);
+  // Molecule mol = parseSDF("./resources/mol.sdf");
+  Molecule mol = parseSDF("./resources/methyl-vinyl-ketone.sdf");
+  // normalizeCoordinates(&mol, screenWidth, screenHeight);
 
-    D printf("Number of atoms: %d\n", mol.num_atoms);
+  D printf("Number of atoms: %d\n", mol.num_atoms);
+  for (int i = 0; i < mol.num_atoms; i++) {
+    D printf("Atom %d: %s (%.4f, %.4f, %.4f) - neighbours: %d\n", i + 1,
+             mol.atoms[i].atom_type, mol.atoms[i].x, mol.atoms[i].y,
+             mol.atoms[i].z, mol.atoms[i].num_neighbours);
+  }
+
+  InitWindow(screenWidth, screenHeight, "LOL dongs");
+
+  SetTargetFPS(60); // Set desired framerate (frames-per-second)
+
+  Camera3D camera = {0};
+  camera.position = (Vector3){10.0f, 10.0f, 10.0f}; // Set camera position
+  camera.target = (Vector3){5.0f, 5.0f, 5.0f};      // Set camera target
+  camera.up = (Vector3){0.0f, 1.0f, 0.0f};          // Set camera up vector
+  camera.fovy = 45.0f;                              // Set camera field of view
+  // camera.projection = CAMERA_ORTHOGRAPHIC;             // Camera projection
+  // type
+  camera.projection = CAMERA_PERSPECTIVE; // Camera projection type
+                                          //
+  int cameraMode = CAMERA_THIRD_PERSON;
+
+  //--------------------------------------------------------------------------------------
+
+  // Main game loop
+  while (!WindowShouldClose()) // Detect window close button or ESC key
+  {
+    // Update
+    //----------------------------------------------------------------------------------
+    if (IsKeyPressed(KEY_ONE)) {
+      cameraMode = CAMERA_FREE;
+      camera.up = (Vector3){0.0f, 1.0f, 0.0f}; // Reset roll
+    }
+
+    if (IsKeyPressed(KEY_TWO)) {
+      cameraMode = CAMERA_FIRST_PERSON;
+      camera.up = (Vector3){0.0f, 1.0f, 0.0f}; // Reset roll
+    }
+
+    if (IsKeyPressed(KEY_THREE)) {
+      cameraMode = CAMERA_THIRD_PERSON;
+      camera.up = (Vector3){0.0f, 1.0f, 0.0f}; // Reset roll
+    }
+
+    if (IsKeyPressed(KEY_FOUR)) {
+      cameraMode = CAMERA_ORBITAL;
+      camera.up = (Vector3){0.0f, 1.0f, 0.0f}; // Reset roll
+    }
+
+    if (IsKeyPressed('Z'))
+      camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+
+    UpdateCamera(&camera, cameraMode);
+    // UpdateCamera(&camera, CAMERA_ORBITAL);
+
+    //----------------------------------------------------------------------------------
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    BeginMode3D(camera);
+
+    // Draw atoms
     for (int i = 0; i < mol.num_atoms; i++) {
-        D printf(
-            "Atom %d: %s (%.4f, %.4f, %.4f) - neighbours: %d\n",
-            i+1, mol.atoms[i].atom_type, mol.atoms[i].x, mol.atoms[i].y, mol.atoms[i].z, mol.atoms[i].num_neighbours
-        );
+      drawAtom(mol.atoms[i]);
+
+      Color bondColor = GREEN;
+
+      for (int j = 0; j < mol.atoms[i].num_neighbours; j++) {
+        if (!mol.atoms[i].bonds_drawn) {
+          if (mol.atoms[i].bond_orders[j] > 1) {
+            bondColor = BLUE;
+          }
+
+          DrawCylinderWiresEx(
+              (Vector3){mol.atoms[i].x, mol.atoms[i].y, mol.atoms[i].z},
+              (Vector3){mol.atoms[i].neighbours[j]->x,
+                        mol.atoms[i].neighbours[j]->y,
+                        mol.atoms[i].neighbours[j]->z},
+              0.1f, 0.1f, 20, bondColor);
+        }
+        mol.atoms[i].bonds_drawn = true;
+      }
     }
 
-    InitWindow(screenWidth, screenHeight, "LOL dongs");
+    EndMode3D();
 
-    SetTargetFPS(60);               // Set desired framerate (frames-per-second)
-    
-    
-    Camera3D camera = { 0 };
-    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Set camera position
-    camera.target = (Vector3){ 5.0f, 5.0f, 5.0f };   // Set camera target
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };       // Set camera up vector
-    camera.fovy = 45.0f;                             // Set camera field of view
-    // camera.projection = CAMERA_ORTHOGRAPHIC;             // Camera projection type
-    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
-                                                        //
-    int cameraMode = CAMERA_THIRD_PERSON;
+    // Print atom coordinates
+    DrawText("Atom Coordinates:", 20, 20, 20, DARKGRAY);
+    for (int i = 0; i < mol.num_atoms; i++) {
+      DrawText(TextFormat("Atom %d: %s (%.4f, %.4f, %.4f) neighbours: %d",
+                          mol.atoms[i].idx, mol.atoms[i].atom_type,
+                          mol.atoms[i].x, mol.atoms[i].y, mol.atoms[i].z,
+                          mol.atoms[i].num_neighbours),
+               20, 40 + i * 10, 8, DARKGRAY);
 
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        if (IsKeyPressed(KEY_ONE))
-        {
-            cameraMode = CAMERA_FREE;
-            camera.up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll
-        }
-
-        if (IsKeyPressed(KEY_TWO))
-        {
-            cameraMode = CAMERA_FIRST_PERSON;
-            camera.up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll
-        }
-
-        if (IsKeyPressed(KEY_THREE))
-        {
-            cameraMode = CAMERA_THIRD_PERSON;
-            camera.up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll
-        }
-
-        if (IsKeyPressed(KEY_FOUR))
-        {
-            cameraMode = CAMERA_ORBITAL;
-            camera.up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll
-        }
-        
-        if (IsKeyPressed('Z')) camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-        
-        UpdateCamera(&camera, cameraMode);
-        // UpdateCamera(&camera, CAMERA_ORBITAL);
-
-
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            BeginMode3D(camera);
-
-                // Draw atoms
-                for (int i = 0; i < mol.num_atoms; i++) {
-                    drawAtom(mol.atoms[i]);
-                    
-                    Color bondColor = GREEN;
-
-                    for (int j = 0; j < mol.atoms[i].num_neighbours; j++) {
-                        if (!mol.atoms[i].bonds_drawn) {
-                            if (mol.atoms[i].bond_orders[j] > 1) {
-                                bondColor = BLUE;
-                            }
-
-                            DrawCylinderWiresEx(
-                                (Vector3){ mol.atoms[i].x, mol.atoms[i].y, mol.atoms[i].z },
-                                (Vector3){ mol.atoms[i].neighbours[j]->x, mol.atoms[i].neighbours[j]->y, mol.atoms[i].neighbours[j]->z },
-                                0.1f, 0.1f, 20, bondColor
-                            );
-                        }
-                        mol.atoms[i].bonds_drawn = true;
-                    }
-                }
-
-            EndMode3D();
-
-            // Print atom coordinates
-            DrawText("Atom Coordinates:", 20, 20, 20, DARKGRAY);
-            for (int i = 0; i < mol.num_atoms; i++) {
-                DrawText(
-                    TextFormat(
-                        "Atom %d: %s (%.4f, %.4f, %.4f) neighbours: %d",
-                        mol.atoms[i].idx, mol.atoms[i].atom_type, mol.atoms[i].x, mol.atoms[i].y, mol.atoms[i].z, mol.atoms[i].num_neighbours
-                    ), 20, 40 + i * 10, 8, DARKGRAY
-                );
-
-            // Convenient place to reset the bond drawn flags
-            mol.atoms[i].bonds_drawn = false;
-            }
-
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+      // Convenient place to reset the bond drawn flags
+      mol.atoms[i].bonds_drawn = false;
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
+    EndDrawing();
+    //----------------------------------------------------------------------------------
+  }
 
-    // TODO: Unload all loaded data (textures, fonts, audio) here!
+  // De-Initialization
+  //--------------------------------------------------------------------------------------
 
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+  // TODO: Unload all loaded data (textures, fonts, audio) here!
 
-    return 0;
+  CloseWindow(); // Close window and OpenGL context
+  //--------------------------------------------------------------------------------------
+
+  return 0;
 }
 
-void drawAtom(Atom atom)
-{
-    Color color;
-    float radius;
+void drawAtom(Atom atom) {
+  Color color;
+  float radius;
 
-    if (strcmp(atom.atom_type, "C") == 0) {
-        color = BLACK;
-        radius = 70.0f/100;
-    } else if (strcmp(atom.atom_type, "O") == 0) {
-        color = RED;
-        radius = 60.0f/100;
-    } else if (strcmp(atom.atom_type, "H") == 0) {
-        color = LIGHTGRAY;
-        radius = 25.0f/100;
-    } else {
-        color = GRAY;
-        radius = 25.0f/100;
-    }
+  if (strcmp(atom.atom_type, "C") == 0) {
+    color = BLACK;
+    radius = 70.0f / 100;
+  } else if (strcmp(atom.atom_type, "O") == 0) {
+    color = RED;
+    radius = 60.0f / 100;
+  } else if (strcmp(atom.atom_type, "H") == 0) {
+    color = LIGHTGRAY;
+    radius = 25.0f / 100;
+  } else {
+    color = GRAY;
+    radius = 25.0f / 100;
+  }
 
-    DrawSphereWires((Vector3){atom.x, atom.y, atom.z}, radius, 25, 25, color);
-} 
+  DrawSphereWires((Vector3){atom.x, atom.y, atom.z}, radius, 25, 25, color);
+}
--- sdfparse.c
+++ sdfparse.c
@@ -1,110 +1,117 @@
+#include "utils.h"
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "utils.h"
 
 #define MAX_ATOMS 1000
 #define MAX_LINE_LENGTH 256
 #define MAX_NEIGHBOURS 4
 
 typedef struct Atom {
-    float x, y, z;
-    char atom_type[3];
-    int idx;
-    int num_neighbours;
-    int bond_orders[MAX_NEIGHBOURS];
-    struct Atom* neighbours[MAX_NEIGHBOURS];
-    bool bonds_drawn;
+  float x, y, z;
+  char atom_type[3];
+  int idx;
+  int num_neighbours;
+  int bond_orders[MAX_NEIGHBOURS];
+  struct Atom *neighbours[MAX_NEIGHBOURS];
+  bool bonds_drawn;
 } Atom;
 
 typedef struct Molecule {
-    Atom atoms[MAX_ATOMS];
-    int num_atoms;
+  Atom atoms[MAX_ATOMS];
+  int num_atoms;
 } Molecule;
 
 void addBond(Atom *atom1, Atom *atom2, int bond_order);
 
 Molecule parseSDF(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        fprintf(stderr, "Error opening file %s\n", filename);
-        exit(1);
+  FILE *file = fopen(filename, "r");
+  if (!file) {
+    fprintf(stderr, "Error opening file %s\n", filename);
+    exit(1);
+  }
+
+  Molecule mol;
+  mol.num_atoms = 0;
+
+  char line[MAX_LINE_LENGTH];
+  while (fgets(line, MAX_LINE_LENGTH, file) != NULL) {
+    // Parse the atoms and their coordinates
+    if (isdigit(line[4]) && isdigit(line[6]) && isalpha(line[31])) {
+      sscanf(line, "\t%f\t%f\t%f\t%s", &mol.atoms[mol.num_atoms].x,
+             &mol.atoms[mol.num_atoms].y, &mol.atoms[mol.num_atoms].z,
+             mol.atoms[mol.num_atoms].atom_type);
+      mol.atoms[mol.num_atoms].idx = mol.num_atoms + 1;
+      mol.atoms[mol.num_atoms].bonds_drawn = false;
+      ++mol.num_atoms;
     }
-
-    Molecule mol;
-    mol.num_atoms = 0;
-
-    char line[MAX_LINE_LENGTH];
-    while (fgets(line, MAX_LINE_LENGTH, file) != NULL) {
-      // Parse the atoms and their coordinates
-        if (isdigit(line[4]) && isdigit(line[6]) && isalpha(line[31])) {
-            sscanf(line, "\t%f\t%f\t%f\t%s", &mol.atoms[mol.num_atoms].x,
-                   &mol.atoms[mol.num_atoms].y, &mol.atoms[mol.num_atoms].z,
-                   mol.atoms[mol.num_atoms].atom_type);
-            mol.atoms[mol.num_atoms].idx = mol.num_atoms + 1;
-            mol.atoms[mol.num_atoms].bonds_drawn = false;
-            ++mol.num_atoms;
-        }
-      // Parse the connectivity info block, atom count in SDF starts at 1
-        if (isdigit(line[2]) && isdigit(line[20])) {
-          int atom_idx, neighbour_idx, bond_order;
-          sscanf(line, "%d %d %d", &atom_idx, &neighbour_idx, &bond_order);
-          D printf("idx %d, buurman %d, multipliciteit %d\n", atom_idx, neighbour_idx, bond_order);
-          addBond(&mol.atoms[atom_idx - 1], &mol.atoms[neighbour_idx - 1], bond_order);
-        }
+    // Parse the connectivity info block, atom count in SDF starts at 1
+    if (isdigit(line[2]) && isdigit(line[20])) {
+      int atom_idx, neighbour_idx, bond_order;
+      sscanf(line, "%d %d %d", &atom_idx, &neighbour_idx, &bond_order);
+      D printf("idx %d, buurman %d, multipliciteit %d\n", atom_idx,
+               neighbour_idx, bond_order);
+      addBond(&mol.atoms[atom_idx - 1], &mol.atoms[neighbour_idx - 1],
+              bond_order);
     }
+  }
 
-    if (fclose(file)) {
-      fprintf(stderr, "Error CLOSING file %s for some reason!\n", filename);
-      exit(1);
-    };
+  if (fclose(file)) {
+    fprintf(stderr, "Error CLOSING file %s for some reason!\n", filename);
+    exit(1);
+  };
 
-    return mol;
+  return mol;
 }
 
 void addBond(Atom *atom1, Atom *atom2, int bond_order) {
-    if (atom1->num_neighbours < MAX_NEIGHBOURS && atom2->num_neighbours < MAX_NEIGHBOURS) {
-        atom1->neighbours[atom1->num_neighbours] = atom2;
-        atom1->bond_orders[atom1->num_neighbours] = bond_order;
-        atom1->num_neighbours++;
+  if (atom1->num_neighbours < MAX_NEIGHBOURS &&
+      atom2->num_neighbours < MAX_NEIGHBOURS) {
+    atom1->neighbours[atom1->num_neighbours] = atom2;
+    atom1->bond_orders[atom1->num_neighbours] = bond_order;
+    atom1->num_neighbours++;
 
-        atom2->neighbours[atom2->num_neighbours] = atom1;
-        atom2->bond_orders[atom2->num_neighbours] = bond_order;
-        atom2->num_neighbours++;
-    } else {
-        D printf("ERROR: MAX_NEIGHBOURS exceeded!\n");
-    }
+    atom2->neighbours[atom2->num_neighbours] = atom1;
+    atom2->bond_orders[atom2->num_neighbours] = bond_order;
+    atom2->num_neighbours++;
+  } else {
+    D printf("ERROR: MAX_NEIGHBOURS exceeded!\n");
+  }
 }
 
 void normalizeCoordinates(Molecule *mol, int width, int height) {
-    float min_x = mol->atoms[0].x, max_x = mol->atoms[0].x;
-    float min_y = mol->atoms[0].y, max_y = mol->atoms[0].y;
-    float min_z = mol->atoms[0].z, max_z = mol->atoms[0].z;
+  float min_x = mol->atoms[0].x, max_x = mol->atoms[0].x;
+  float min_y = mol->atoms[0].y, max_y = mol->atoms[0].y;
+  float min_z = mol->atoms[0].z, max_z = mol->atoms[0].z;
 
-    // Find min and max coords
-    for (int i = 1; i < mol->num_atoms; i++) {
-        if (mol->atoms[i].x < min_x) min_x = mol->atoms[i].x;
-        if (mol->atoms[i].x > max_x) max_x = mol->atoms[i].x;
-        if (mol->atoms[i].y < min_y) min_y = mol->atoms[i].y;
-        if (mol->atoms[i].y > max_y) max_y = mol->atoms[i].y;
-        if (mol->atoms[i].z < min_z) min_z = mol->atoms[i].z;
-        if (mol->atoms[i].z > max_z) max_z = mol->atoms[i].z;
-    }
+  // Find min and max coords
+  for (int i = 1; i < mol->num_atoms; i++) {
+    if (mol->atoms[i].x < min_x)
+      min_x = mol->atoms[i].x;
+    if (mol->atoms[i].x > max_x)
+      max_x = mol->atoms[i].x;
+    if (mol->atoms[i].y < min_y)
+      min_y = mol->atoms[i].y;
+    if (mol->atoms[i].y > max_y)
+      max_y = mol->atoms[i].y;
+    if (mol->atoms[i].z < min_z)
+      min_z = mol->atoms[i].z;
+    if (mol->atoms[i].z > max_z)
+      max_z = mol->atoms[i].z;
+  }
 
-    // Scaling factors
-    float scale_x = (float)width / (max_x - min_x) / 100;
-    float scale_y = (float)height / (max_y - min_y) / 100;
-    // Yeah well there is no depth to a screen lol
-    float scale_z = (float)height / (max_z - min_z) / 100;
+  // Scaling factors
+  float scale_x = (float)width / (max_x - min_x) / 100;
+  float scale_y = (float)height / (max_y - min_y) / 100;
+  // Yeah well there is no depth to a screen lol
+  float scale_z = (float)height / (max_z - min_z) / 100;
 
-    // Normalize coordinates, lets hope we never need the original coords lmao
-    for (int i = 0; i < mol->num_atoms; i++) {
-        mol->atoms[i].x = (mol->atoms[i].x - min_x) * scale_x;
-        mol->atoms[i].y = (mol->atoms[i].y - min_y) * scale_y;
-        mol->atoms[i].z = (mol->atoms[i].z - min_z) * scale_z;
-    }
-
+  // Normalize coordinates, lets hope we never need the original coords lmao
+  for (int i = 0; i < mol->num_atoms; i++) {
+    mol->atoms[i].x = (mol->atoms[i].x - min_x) * scale_x;
+    mol->atoms[i].y = (mol->atoms[i].y - min_y) * scale_y;
+    mol->atoms[i].z = (mol->atoms[i].z - min_z) * scale_z;
+  }
 }
-
